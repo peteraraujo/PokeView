75b8a8b1ba7255539001c02565b4b6529aae58fe
Peter Araujo <contact@peteraraujo.com>
2026-06-13 07:58:18 -0400

    Updated lint rules on .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    // ...other rules
+    'react/no-unstable-nested-components': [
+      'warn', // or 'error'
+      { allowAsProps: true }
+    ],
+  },
 };
